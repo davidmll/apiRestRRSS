2a814535588daf9c029661d669fd8ab30bc43a65
davidmll <david.martinez-llano@capgemini.com>
2022-03-03 09:03:45 +0100

    feat(master) commit base

---
+++ apirest-RRHH/src/main/java/com/projecto/java/ApirestRrhhApplication.java
@@ -0,0 +1,13 @@
+package com.projecto.java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApirestRrhhApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApirestRrhhApplication.class, args);
+	}
+
+}
